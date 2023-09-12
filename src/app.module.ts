941f8b3ef64713c52d0dd0f72c9731fb7ef2e2f0
MarkPoloChina <1228479724@qq.com>
2023-09-12 18:22:08 +0800

    [update] to sqllite

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,14 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IllustModule } from './illust/illust.module';
 import { PixivApiModule } from './pixiv-api/pixiv-api.module';
-import config from './config';
 
 @Module({
   imports: [
     IllustModule,
     PixivApiModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: config.database.host,
-      port: 3306,
-      username: config.database.user,
-      password: config.database.password,
-      database: config.database.database,
+      type: 'sqlite',
+      database: 'test.db',
       autoLoadEntities: true,
       synchronize: true, // 数据库自动同步 entity 文件修改
     }),
